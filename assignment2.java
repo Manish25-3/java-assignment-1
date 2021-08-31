8d85b4db17e8ccf963d3b465d2c239ec30ea5915
Manish25-3 <84845960+Manish25-3@users.noreply.github.com>
2021-08-31 22:56:26 +0530

    day2 assignment2 submitted

---
+++ assignment2.java
@@ -0,0 +1,18 @@
+class sum
+{ public static void main(String ar[])
+      {     int x,y,s;
+            x=Integer.parseInt(ar[0]);
+            y=Integer.parseInt(ar[1]);
+            s=x+y;
+            System.out.println("sum of " + x + " and " + y +" is " +s);
+      }
+}
+
+/*
+output : 
+ 
+compile : PS C:\Users\ASUS\Desktop\Java - LetsUpgrade> javac assignment2.java
+execute : PS C:\Users\ASUS\Desktop\Java - LetsUpgrade> java sum 2 3
+ 
+               sum of 2 and 3 is 5
+*/               
